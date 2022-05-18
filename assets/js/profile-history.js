9a127e87353cfa2c2c3ce7477bf0bddf3ad51b57
Hanif-tech <hanif.rifqi17@gmail.com>
2022-05-18 20:43:33 +0700

    done revisi ke1

---
+++ assets/js/profile-history.js
@@ -0,0 +1,7 @@
+$(document).on("click", ".img-ud", function (e) {
+  $(".hide").addClass("d-none");
+  const g = document.querySelectorAll(".hide");
+  // $(x).addClass("d-none").siblings().removeClass("d-none");
+
+  console.log(g);
+});
